558d3d47026318ab051ffcc72ee385a0ab69b22a
noodleswww <boatwww@icloud.com>
2016-06-06 11:54:52 +0800

    daily contributing

---
+++ crontab.sh
@@ -0,0 +1,8 @@
+
+
+# cd /Users/company/github/keep-contributing/ && date | cat >> test.js && git commit -a -m 'daily contributing' && git push origin master >> /var/log/crontab/keep-contributing.log 2>&1
+cd /Users/company/github/keep-contributing/ 
+date | cat >> test.js 
+git add .
+git commit -m 'daily contributing'
+git push origin master 
